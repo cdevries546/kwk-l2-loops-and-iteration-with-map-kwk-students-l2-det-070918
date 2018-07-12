105dc27b4c27ef50a6774fb7fe028ea6202ce706
cdevries546 <cdevries@comcast.net>
2018-07-12 17:32:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+// var gifts =['drone', 'Jordans', 'clothes', 'corgi', 'books','pomsky']
+// function wrapGift(gifts) {
+//   console.log(gifts+ "has been wrapped")
+// }
+// for (var counter = 0; counter < gifts.length; counter++){
+//   var gift =gifts[counter]
+//   wrapGift(gift)
+// }
+//for=like the number of times
+// function wrapGift(gift) {
+//   console.log("Wrapped" + gift)
+// }
+// var gifts = ['teddy bear', 'drone', 'doll']
+// gifts.forEach(wrapGift)
+
+
+
+
+var lowerCaseStudentNames =['Adele', 'Beyonce', 'Lady', 'Madonna', 'Rihanna', 'Taylor']
